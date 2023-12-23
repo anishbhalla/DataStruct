e5d2574f1af1bbe2ad7d0b49bead7d9998ebf642
Anish Bhalla <139158263+anishbhalla@users.noreply.github.com>
2023-12-24 00:06:56 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int binarysearch(int arr[],int n,int key){
     int s=0;
     int e=n-1;
-    int mid=(s+e)/2;
+    int mid=(s+e)/2; //OR mid=s+(e-s)/2
     while (s<=e){
         if (arr[mid]==key){ //If 'mid' element is equal to 'key', return 'mid' index.
             return mid;
@@ -15,6 +15,7 @@ int binarysearch(int arr[],int n,int key){
         else{ // Go to left part of 'mid' element.
             s=mid+1;
         }
+        mid=(s+e)/2; //OR mid=s+(e-s)/2
     }
     return -1; // Return -1 if 'key' not present in array.
 }
